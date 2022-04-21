483f3059a2cd51209a74e2098876cc505f170c43
joactr <j.m.molol@hotmail.com>
2022-04-21 09:27:09 +0200

    promesa reject

--- funcSQL.js
+++ funcSQL.js
@@ -38,7 +38,7 @@ async function login(params){
         ps.prepare(`SELECT * FROM users WHERE username = @username`, err => {
             if(err){console.log(err);reject(err);}
           ps.execute({username: params.username}, (err, result) => {
-            if(err){console.log(err);console.log("ERROR DE TIMEOUT") reject(err);}
+            if(err){console.log(err);console.log("ERROR DE TIMEOUT"); reject(err);}
             resolve(result.recordsets)
           })
         })
